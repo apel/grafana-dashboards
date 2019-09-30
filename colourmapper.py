d8300c4e9eed0dfe5bcb431ebeec68d4f148467d
tswinb <timothy.swinbank@stfc.ac.uk>
2019-09-30 13:27:03 +0100

    Address feedback including new config file
    Add command line method of specifying filepath. Move list of colour-maps
    to config file. Use read-only file and write to new file.

--- colourmapper.py
+++ colourmapper.py
@@ -5,60 +5,40 @@ The script is currently set up to set predefined colour mappings for VOs and
 Sites on the IRIS Accounting dashboard. Use Hex codes to avoid issues with pie
 charts.
 
-Usage: 'python colourmapper.py'
-(need to set JSON filepath below)
+Usage: 'python colourmapper.py filepath'
 """
 
 import json
 import os
+import argparse
+import configparser
 
-# Enter the filepath for the JSON file you want to edit
-filepath = 'dashboard.json'
+# retrieve config
+configParser = configparser.RawConfigParser()
+configFilePath = 'colourmapper.ini'
+configParser.read(configFilePath)
+aliasColors = configParser.get('Colours', 'aliasColors')
+
+# retrieve argument (filepath)
+parser = argparse.ArgumentParser()
+parser.add_argument("filepath", help="filepath of JSON file that will be edited")
+args = parser.parse_args()
 
 def main():
     """
     Update the desired JSON file with predefined colors
     """
-    with open(filepath, 'r+') as f:
-        data = json.load(f)
+    filename = args.filepath[:-5] # get filename
+    newPath = filename + str(os.getpid()) + '.json' # create a distinct name for the new file
+    with open(args.filepath, 'r') as reader, open(newPath, 'w') as writer:
+        # open the existing file as read only, and write a new file
+        data = json.load(reader) # load file into data variable
 
         for i in range(len(data['panels'])): # cycle through panels in JSON file
-            data['panels'][i]['aliasColors'] = {"lsst": "#B9D7ED", \
-                                                "lz": "#e61263", \
-                                                "skatelescope.eu": "#DCB9ED", \
-                                                "dune": "#8F2323", \
-                                                "vo.cta.in2p3.fr": "#23628F", \
-                                                "virgo": "#8F6A23", \
-                                                "ISIS": "#4F8F23", \
-                                                "Diamond": "#737373", \
-                                                "CLF": "#FF00AA", \
-                                                "CCFE": "#469990", \
-                                                "EUCLID": "#bfef45", \
-                                                "AENEAS": "#f58231", \
-                                                "eMERLIN": "#ffd8b1", \
-                                                "LSST": "#8bbaf0", \
-                                                \
-                                                "ral-cloud": "#E02F44", \
-                                                "UKI-LT2-IC-HEP": "#FFF899", \
-                                                "UKI-NORTHGRID-MAN-HEP": "#00EAFF", \
-                                                "RAL-LCG2": "#AA00FF", \
-                                                "UKI-LT2-QMUL": "#FF7F00",\
-                                                "UKI-NORTHGRID-LIV-HEP": "#BFFF00", \
-                                                "UKI-SCOTGRID-ECDF": "#0095FF", \
-                                                "UKI-SOUTHGRID-RALPP": "#4F8F23", \
-                                                "UKI-SOUTHGRID-OX-HEP": "#6B238F", \
-                                                "UKI-NORTHGRID-SHEF-HEP": "#FFD400", \
-                                                "UKI-SOUTHGRID-BRIS-HEP": "#6AFF00", \
-                                                "UKI-NORTHGRID-LANCS-HEP": "#EDB9B9", \
-                                                "UKI-LT2-Brunel": "#aaffc3", \
-                                                "UKI-SCOTGRID-GLASGOW": "#000075", \
-                                                "UKI-SOUTHGRID-BHAM-HEP": "#f032e6", \
-                                                "UKI-SOUTHGRID-CAM-HEP": "#808000", \
-                                                "UKI-LT2-RHUL": "#800000"
-                                                }
-        f.seek(0) # overwrite the beginning of the file
-        json.dump(data, f, indent=4) # write
-        f.truncate() # needed if new data is smaller than previous
+            data['panels'][i]['aliasColors'] = aliasColors
+
+        json.dump(data, writer, indent=4) # write to new file
+    os.replace(newPath, args.filepath) # replace new file with original
 
 if __name__ == '__main__':
     main()
