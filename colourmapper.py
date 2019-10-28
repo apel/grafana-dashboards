570aa1a8a05a836c4eaf87a3b8145a572132e53e
tswinb <timothy.swinbank@stfc.ac.uk>
2019-10-28 10:19:55 +0000

    Make requested changes to comments and variable naming
    Change variables to match Python naming conventions. Make 'Usage' 
    clearer. Make comments clearer.

--- colourmapper.py
+++ colourmapper.py
@@ -5,7 +5,7 @@ The script is currently set up to set predefined colour mappings for VOs and
 Sites on the IRIS Accounting dashboard. Use Hex codes to avoid issues with pie
 charts.
 
-Usage: 'python colourmapper.py filepath'
+Usage: 'python colourmapper.py [filepath]'
 """
 
 import json
@@ -14,13 +14,13 @@ import argparse
 import configparser
 
 # retrieve config
-configParser = configparser.RawConfigParser()
-configParser.optionxform = str
-configFilePath = 'colourmapper.ini'
-configParser.read(configFilePath)
-aliasColors = dict(configParser.items('Colours'))
+config_parser = configparser.RawConfigParser()
+config_parser.optionxform = str
+config_file_path = 'colourmapper.ini'
+config_parser.read(config_file_path)
+alias_colors = dict(config_parser.items('Colours'))
 
-# retrieve argument (filepath)
+# parse the commandline arguments to retrieve filepath
 parser = argparse.ArgumentParser()
 parser.add_argument("filepath", help="filepath of JSON file that will be edited")
 args = parser.parse_args()
@@ -30,16 +30,16 @@ def main():
     Update the desired JSON file with predefined colors
     """
     filename = args.filepath[:-5] # get filename
-    newPath = filename + str(os.getpid()) + '.json' # create a distinct name for the new file
-    with open(args.filepath, 'r') as reader, open(newPath, 'w') as writer:
+    new_path = filename + str(os.getpid()) + '.json' # create a distinct name for the new file
+    with open(args.filepath, 'r') as reader, open(new_path, 'w') as writer:
         # open the existing file as read only, and write a new file
         data = json.load(reader) # load file into data variable
 
         for i in range(len(data['panels'])): # cycle through panels in JSON file
-            data['panels'][i]['aliasColors'] = aliasColors
+            data['panels'][i]['aliasColors'] = alias_colors
 
         json.dump(data, writer, indent=4) # write to new file
-    os.replace(newPath, args.filepath) # replace new file with original
+    os.replace(new_path, args.filepath) # replace original file with new file
 
 if __name__ == '__main__':
     main()
