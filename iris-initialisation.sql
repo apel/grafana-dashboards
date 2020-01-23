28c9174d2988a8b8d39d78c4c90e40ff2bd2cb3d
tswinb <18294786+tswinb@users.noreply.github.com>
2020-01-23 11:00:00 +0000

    Add initialisation of Allocations and Capacity

--- iris-initialisation.sql
+++ iris-initialisation.sql
@@ -3,3 +3,15 @@ INSERT INTO IrisVOs VALUES
     ("virgo"),("ISIS"),("Diamond"),("CLF"),("CCFE"),("EUCLID"),("AENEAS"),
     ("eMERLIN"),("dirac"),("jintrac"),("casu"),("gaia"),("gaia-dev"),
     ("gaia-prod"),("gaia-test"),("vcycle"),("iris.ac.uk");
+
+INSERT INTO Capacity VALUES
+    ("2018-01-01", 0),("2018-04-01", 2937),("2019-04-01", 7718);
+
+INSERT INTO Allocations VALUES
+    ("2019-04-01", "lsst", 1312),("2019-04-01", "lz", 300),
+    ("2019-04-01", "skatelescope.eu", 1920),("2019-04-01", "dune", 1000),
+    ("2019-04-01", "vo.cta.in2p3.fr", 285),("2019-04-01", "virgo", 270),
+    ("2019-04-01", "ISIS", 175),("2019-04-01", "Diamond", 2100),
+    ("2019-04-01", "CLF", 68),("2019-04-01", "CCFE", 1250),
+    ("2019-04-01", "EUCLID", 3100),("2019-04-01", "eMERLIN", 96),
+    ("2019-04-01", "jintrac", 200);
