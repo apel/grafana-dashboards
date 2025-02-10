b7417065218b73c5cacd7439b3aa71a60687fb90
William-Brown5515 <william.brown@stfc.ac.uk>
2025-02-10 13:17:15 +0000

    Include capital letter for GlueX, insert in alphabetical order.

--- schemas/iris-initialisation.sql
+++ schemas/iris-initialisation.sql
@@ -1,9 +1,9 @@
 INSERT INTO IrisVOs VALUES
     ('AENEAS'),('casu'),('CCP4'),('CLF'),('Diamond'),
     ('dirac'),('eMERLIN'),('EUCLID'),('eucliduk.net'),('fermilab'),
-    ('gaia'),('gaia-core'),('gaia-dev'),('gaia-prod'),('gaia-test'),
+    ('gaia'),('gaia-core'),('gaia-dev'),('gaia-prod'),('gaia-test'),('Gluex'),
     ('ISIS'),('jintrac'),('lz'),('plato'),('ska'),('skatelescope.eu'),
-    ('ccfe'),('clas12'),('dune'),('glueX'),('lsst'),
+    ('ccfe'),('clas12'),('dune'),('lsst'),
     ('virgo'),('vo.cta.in2p3.fr'),('cta');
 
 INSERT INTO Capacity VALUES
